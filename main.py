35da8451ecf483b5c001a700eb0656b2d7aa4688
Harsh C. Shah <141438175+harshcshah@users.noreply.github.com>
2023-10-25 14:33:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import datetime as dt
-import joblib
+import joblib  # Ensure you have joblib installed
 
 # Function to make sales prediction
 def make_sales_prediction():
@@ -17,14 +17,17 @@ def make_sales_prediction():
     p4 = options1.index(outlet_type)
     
     if st.button('Predict'):
-        model = joblib.load('bigmart_model')
         result = model.predict(np.array([[p1, p2, p3, p4, p5]]))
         st.write("Sales amount", result)
 
+# Your code for loading the model and making predictions
 current_year = dt.datetime.today().year
 options = ['OUT010', 'OUT013', 'OUT017', 'OUT018', 'OUT019', 'OUT027', 'OUT035', 'OUT045', 'OUT046', 'OUT049']
 options0 = ['High', 'Medium', 'Small']
 options1 = ['Grocery Store', 'Supermarket Type1', 'Supermarket Type2', 'Supermarket Type3']
+
+# Load your model here
+model = None  # Replace with the code to load your model
 
 st.title("Big Mart Sales Prediction using Machine Learning")
 
