963c44c23b681a5b4c9f42701c89ca6f0172a65b
Harsh C. Shah <141438175+harshcshah@users.noreply.github.com>
2023-10-25 14:16:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 import streamlit as st
-import joblib
 import numpy as np
 import datetime as dt
+import joblib
+
+# Function to make sales prediction
+def make_sales_prediction():
+    p1 = float(st.text_input("Item_MRP"))
+    p5 = current_year - int(st.text_input("Outlet_Establishment_Year"))
+    
+    outlet_identifier = st.selectbox("Outlet Identifier", ['OUT010', 'OUT013', 'OUT017', 'OUT018', 'OUT019', 'OUT027', 'OUT035', 'OUT045', 'OUT046', 'OUT049'])
+    outlet_size = st.selectbox("Outlet Size", ['High', 'Medium', 'Small'])
+    outlet_type = st.selectbox("Outlet Type", ['Grocery Store', 'Supermarket Type1', 'Supermarket Type2', 'Supermarket Type3'])
+    
+    p2 = options.index(outlet_identifier)
+    p3 = options0.index(outlet_size)
+    p4 = options1.index(outlet_type)
+    
+    if st.button('Predict'):
+        model = joblib.load('bigmart_model')
+        result = model.predict(np.array([[p1, p2, p3, p4, p5]]))
+        st.write("Sales amount", result)
 
 current_year = dt.datetime.today().year
+options = ['OUT010', 'OUT013', 'OUT017', 'OUT018', 'OUT019', 'OUT027', 'OUT035', 'OUT045', 'OUT046', 'OUT049']
+options0 = ['High', 'Medium', 'Small']
+options1 = ['Grocery Store', 'Supermarket Type1', 'Supermarket Type2', 'Supermarket Type3']
 
-st.title("Big Mart Sales Prediction")
+st.title("Big Mart Sales Prediction using Machine Learning")
 
-# Input fields
-item_mrp = st.number_input("Item MRP", min_value=0.0)
-outlet_identifier = st.selectbox("Outlet Identifier", [9.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 0.0])
-outlet_size = st.selectbox("Outlet Size", [1.0, 0.0, 2.0])
-outlet_type = st.selectbox("Outlet Type", [1.0, 2.0, 3.0, 0.0])
-establishment_year = st.number_input("Outlet Establishment Year")
-
-if st.button("Predict"):
-    p1 = item_mrp
-    p2 = outlet_identifier
-    p3 = outlet_size
-    p4 = outlet_type
-    p5 = current_year - establishment_year
-
-    # Load the pre-trained model using joblib
-    model = joblib.load('bigmart_model.pkl')
-
-    result = model.predict(np.array([[p1, p2, p3, p4, p5]])
-
-    # Display the prediction
-    st.write("Predicted Sales: " + str(result[0]))
-    st.write("Sales Value is between " + str(result[0]-714.42) + " and " + str(result[0]+714.42))
+make_sales_prediction()
